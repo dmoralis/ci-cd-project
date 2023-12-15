cf7112546deaf08849e2f012d4fad56467881233
dmoralis <dmoralis@csd.auth.gr>
2023-12-15 10:24:19 +0200

    Changed the ci/cd yaml to build after testing successfully and fixed the tutorial-error in the test.py

--- test.py
+++ test.py
@@ -15,4 +15,4 @@ def client():
 def test_app_working(client):
     response = client.get('/')
     assert response.status_code == 200
-    assert response.data == b"Hello World"
+    assert response.data == b"Hello world"
